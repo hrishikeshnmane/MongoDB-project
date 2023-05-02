f449d0de127ed25a21b74fed52e23191ecda95c1
hrishikeshmane <hrishikeshnmane@gmail.com>
2023-05-02 14:42:41 -0400

    configuring authorization

--- src/main/java/com/springboot/springbootsecurity/SecurityConfiguration.java
+++ src/main/java/com/springboot/springbootsecurity/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.springboot.springbootsecurity;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -19,7 +20,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
                 .withUser("blah")
                 .password("blah")
-                .roles("admin");
+                .roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // configure authorization
+        http.authorizeRequests()
+                .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/user").hasAnyRole("USER","ADMIN")
+                .antMatchers("/").permitAll()
+                .and().formLogin();
     }
 
     @Bean
